bc095be0d0e7b6687466058f46298248be4aed45
dariovasq <dario.vasques@heig-vd.ch>
2023-02-21 15:59:40 +0100

    Changes by calum

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include <vector>
+#include "test.cpp"
 
+using namespace std;
+
+int main() {
+
+   vector<int> vec1 = {1,2,3,4,5};
+   vector<double> vec2 = {1.,3.,2.,4.,5.};
+
+   cout << boolalpha << estTrie(vec1) << endl;
+   cout << boolalpha << estTrie(vec2) << endl;
+
+   return 0;
+}
--- src/test.cpp
+++ src/test.cpp
@@ -5,8 +5,8 @@ using namespace std;
 template <typename T>
 bool estTrie(const vector<T>& vec) {
    if (vec.size())
-      for (size_t i = 0; i < vec.size(); ++i) {
-         if (vec[i] > vec[i + 1])
+      for (size_t i = 1; i < vec.size(); ++i) {
+         if (vec[i - 1] > vec[i])
             return false;
       }
    return true;
