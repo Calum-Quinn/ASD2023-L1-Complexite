e9122269ad9869dbd7b6b67e6e2bf3b22b089caa
dariovasq <dario.vasques@heig-vd.ch>
2023-02-21 15:46:30 +0100

    Changes by calum

--- src/test.cpp
+++ src/test.cpp
@@ -1,1 +1,13 @@
+#include <vector>
 
+using namespace std;
+
+template <typename T>
+bool estTrie(const vector<T>& vec) {
+   if (vec.size())
+      for (size_t i = 0; i <= vec.size(); ++i) {
+         if (vec[i] > vec[i])
+            return false;
+      }
+   return true;
+}
