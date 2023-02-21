de157823e2ebae5c0a898803e338c182248cc602
dariovasq <dario.vasques@heig-vd.ch>
2023-02-21 15:41:01 +0100

    Fonction de tri

--- src/tris.cpp
+++ src/tris.cpp
@@ -1,1 +1,24 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+// Basé sur les exemples de tris du Git PRG1 de M Breguet
+
+
+//---------------------------------------------------------
+// Fonction de tri à bulles
+template <typename T>
+void bubbleSort(vector<T>& v){
+   if (v.size() > 0){
+      for (size_t i = 0; i < v.size() - 1; ++i){
+         for (size_t j = 1; j < v.size() - i; ++j){
+            if (v[j - 1] > v[j]) {
+               swap(v[j - 1], v[j]);
+            }
+         }
+      }
+   }
+}
 
