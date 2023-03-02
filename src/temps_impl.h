24ad2780375c5aa644cffd311d17fa83a305079d
calum <calum.quinn@heig-vd.ch>
2023-03-02 11:21:22 +0100

    Changes by ema

--- src/temps_impl.h
+++ src/temps_impl.h
@@ -15,13 +15,9 @@ using namespace std;
 template<typename T, typename Fn>
 double mesure_temps(vector<T> v1, Fn f) {
     auto t1 = std::chrono::high_resolution_clock::now();
-    //auto r = f(v1.begin(), v1.end());
     f(v1.begin(), v1.end());
     auto t2 = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double, std::milli> time_ms = t2 - t1;
-/*    cout << "f(" << "v" << ") = " << r
-         << " en " << time_ms.count() << " ms "
-         << endl;*/
 
     return (double)time_ms.count();
 }
