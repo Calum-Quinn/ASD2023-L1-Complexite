41373cc5f13ba439471a1f088ddfd4ff5db876d6
dariovasq <dario.vasques@heig-vd.ch>
2023-03-02 11:32:35 +0100

    Correction

--- src/temps_impl.h
+++ src/temps_impl.h
@@ -9,11 +9,8 @@
 #include <iostream>
 #include "tris.h"
 
-
-using namespace std;
-
 template<typename T, typename Fn>
-double mesure_temps(vector<T> v1, Fn f) {
+double mesure_temps(std::vector<T> v1, Fn f) {
     auto t1 = std::chrono::high_resolution_clock::now();
     //auto r = f(v1.begin(), v1.end());
     f(v1.begin(), v1.end());
