5b76739e7b337197b190dbb0dfc9c82735b966c6
calum <calum.quinn@heig-vd.ch>
2023-03-02 10:10:55 +0100

    Utiliser algorithm pour le test de tri

--- src/test.h
+++ src/test.h
@@ -1,11 +1,11 @@
 #ifndef ASD2022_L1_COMPLEXITE_TEST_H
 #define ASD2022_L1_COMPLEXITE_TEST_H
 
+#include <algorithm>
+
 template <typename T>
-bool estTrie(const std::vector<T>& vec);
-
-
-
-#include "test_impl.h"
+bool estTrie(const std::vector<T>& vec) {
+    return std::is_sorted(vec.begin(), vec.end());
+}
 
 #endif //ASD2022_L1_COMPLEXITE_TEST_H
