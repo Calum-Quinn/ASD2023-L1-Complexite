6152a1f1d3ad93c9ecc3b39c7a42766ecd2be910
calum <calum.quinn@heig-vd.ch>
2023-03-04 12:22:15 +0100

    Generation des vecteurs presque triés

--- src/generateVector_impl.h
+++ src/generateVector_impl.h
@@ -3,45 +3,38 @@
 
 #include <random>
 
+
 template <typename T>
 std::vector<T> generateVector(size_t n, int seed, typeTri tri) {
    std::vector<T> vecteur(n);
 
    if(tri == typeTri::CROISSANT) {
       for (size_t i = 0; i < vecteur.size(); ++i) {
-         vecteur[i] = i;
+         vecteur[i] = T(i);
       }
    }
 
    if(tri == typeTri::DECROISSANT) {
       for (size_t i = 0; i < vecteur.size(); ++i) {
-         vecteur[i] = n - (i + 1);
+         vecteur[i] = T(n - (i + 1));
       }
    }
 
    if (tri == typeTri::ALEATOIRE) {
        for (size_t i = 0; i < vecteur.size(); ++i) {
-           vecteur[i] = i;
+           vecteur[i] = T(i);
        }
        shuffle(vecteur.begin(), vecteur.end(), default_random_engine(seed));
    }
 
    if (tri == typeTri::PRESQUETRIE) {
+       srand(seed);
        for (size_t i = 0; i < vecteur.size(); ++i) {
-           srand(seed);
-           vecteur[i] = int(double(i)/2. + double(rand()%101)/2.); // Somme pondérée entre tri croissant et aléatoire
+           vecteur[i] = size_t(double(i)/2. + double(rand()%n)/2.); // Somme pondérée entre tri croissant et aléatoire
        }
    }
 
    return vecteur;
-}
-
-int nbAleatoire(int valMin, int valMax) {
-   random_device rand_dev;
-   default_random_engine generator(rand_dev());
-   uniform_int_distribution<int> distr(valMin, valMax);
-
-   return distr(generator);
 }
 
 #endif //ASD2022_L1_COMPLEXITE_GENERATEVECTOR_IMPL_H
